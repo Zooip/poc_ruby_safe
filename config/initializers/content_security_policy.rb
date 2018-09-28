2231239ef74d821c7504d084dc5038ac382d72bb
Alexandre Narbonne <alexandre.narbonne@yahoo.fr>
2018-09-28 09:11:26 +0200

    Fix webpack-dev-server

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,6 +17,7 @@
 
    if Rails.env.development?
      policy.script_src :self, :https, :unsafe_eval
+     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
    else
      policy.script_src :self, :https
    end
